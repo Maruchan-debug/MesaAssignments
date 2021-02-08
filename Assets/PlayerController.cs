e87427fb4b95c1e6f20cf24e9db5aa301a50ad75
SDMesaAssignments <60202182+SDMesa>
2021-02-08 05:49:57 -0800

    MCasillas_Unit1LessonB Update
    The trick is to go fast and fully commit!

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Moves the transform in the direction and distance of translation//
+
+        //.foward in Vector3 for forward new vector movement lol GG//
+
+        Debug.Log(Time.deltaTime);
+        //Vector3.forward ---> (0,0,1)
+        //(0,0,1) * Time.deltaTime = (0,0,.001)
+        transform.Translate(Vector3.forward * Time.deltaTime * 10);
+
+
+    }
+}
